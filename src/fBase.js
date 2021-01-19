dc5152866185b0801076c032654c61a7441870d0
egs125 <egs125@gmail.com>
2021-01-19 22:28:32 +0900

    use Auth & make Form

--- src/fBase.js
+++ src/fBase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -7,12 +8,8 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  //   apiKey: "AIzaSyDhyqiBB3WqLEcF5wPKiY4FSJYekEVohCo",
-  //   authDomain: "nwitter-5661b.firebaseapp.com",
-  //   projectId: "nwitter-5661b",
-  //   storageBucket: "nwitter-5661b.appspot.com",
-  //   messagingSenderId: "43842432441",
-  //   appId: "1:43842432441:web:fb661d42eca72e8b709a68",
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
