953460e392ce8c6050300a02a9d4f2650dbc0d44
egs125 <egs125@gmail.com>
2021-03-01 16:25:59 +0900

    add getting the list of nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fBase";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection('nweets').get();
+    const tempArray = [];
+    
+    dbNweets.forEach(document => {
+        const nweetObj = {
+            ...document.data(),
+            id: document.id,
+        };
+        // setNweets(prev => [nweetObj, ...prev]);
+        tempArray.push(nweetObj);
+    });
+
+    setNweets(tempArray);
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -35,6 +56,10 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+
+      <div>
+        {nweets.map(nweet => <div key={nweet.id}><h4>{nweet.nweet}</h4></div>)}
+      </div>
     </div>
   );
 };
